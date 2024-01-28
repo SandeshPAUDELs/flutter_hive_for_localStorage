61458eb911cb1ec9f6610781c8538d54f6ff42b4
Sandesh Paudel <sandeshpaudle017@gmai.com>
2024-01-28 14:16:58 +0545

    first commit

---
+++ android/app/src/main/kotlin/com/example/hieve_storage/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hieve_storage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:hieve_storage/home.dart';
+
+void main() async {
+  await Hive.initFlutter();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hieve Storage App',
+      home: HomeScreen(),
+    );
+  }
+}
